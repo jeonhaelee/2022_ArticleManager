b4889feaf758453da7c9ddeef9ee029313495fe2
jeonhaelee <jhealee0624@gmail.com>
2022-02-21 21:07:32 +0900

    게시물 프로그램1

---
+++ src/com/koreaIT/java/AM/Main.java
@@ -0,0 +1,14 @@
+package com.koreaIT.java.AM;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.koreaIT.java.AM.dto.Article;
+import com.koreaIT.java.AM.util.Util;
+
+public class Main {
+	public static void main(String[] args) {
+		new App().start();
+	}
+}
